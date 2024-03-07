604211e2b8c76409c9891edf34061e489e6b35a4
Marcin Szawerda <90306453+Marcin658@users.noreply.github.com>
2024-03-07 13:53:18 +0100

    Add files via upload

---
+++ Panel/Index.php
@@ -0,0 +1,17 @@
+<?php
+require_once '../Baza_Danych/db.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styl/styl.css">
+    <title>Muzyka</title>
+</head>
+<body>
+    
+</body>
+</html>
