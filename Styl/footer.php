1146a9e35684c899ec78dee3baa77ef8b638ce00
Marcin Szawerda <90306453+Marcin658@users.noreply.github.com>
2024-05-31 17:54:15 +0200

    Add files via upload

--- Styl/footer.php
+++ Styl/footer.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="pl">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../Styl/footer.css">
-    <title>Document</title>
-</head>
-<body>
+<link rel="stylesheet" href="../Styl/footer.css">
+
+
 <p>Stronę Wykonał Marcin Szawerda &copy;</p>
-</body>
-</html>
